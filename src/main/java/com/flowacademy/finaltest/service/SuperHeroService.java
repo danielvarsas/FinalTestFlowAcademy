2f258744be68500cc4b167b97c583764b347bb07
Daniel Varsas <daniel.varsas@gmail.com>
2021-02-08 11:01:07 +0100

    Inject repos into services'

--- src/main/java/com/flowacademy/finaltest/service/SuperHeroService.java
+++ src/main/java/com/flowacademy/finaltest/service/SuperHeroService.java
@@ -1,5 +1,18 @@
 package com.flowacademy.finaltest.service;
 
+import com.flowacademy.finaltest.repository.SuperHeroRepository;
+import com.flowacademy.finaltest.repository.TeamRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SuperHeroService {
-    //test
+
+    private static final Logger log = LoggerFactory.getLogger(SuperHeroService.class);
+    private final SuperHeroRepository superHeroRepository;
+
+    public SuperHeroService(SuperHeroRepository superHeroRepository) {
+        this.superHeroRepository = superHeroRepository;
+    }
+
+
 }
--- src/main/java/com/flowacademy/finaltest/service/TeamService.java
+++ src/main/java/com/flowacademy/finaltest/service/TeamService.java
@@ -1,7 +1,17 @@
 package com.flowacademy.finaltest.service;
 
+import com.flowacademy.finaltest.repository.TeamRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TeamService {
 
+    private static final Logger log = LoggerFactory.getLogger(TeamService.class);
+    private final TeamRepository teamRepository;
+
+    public TeamService(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+    }
 
 
 }
